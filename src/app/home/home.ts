a38a0332c8b4f9121457161756228e02cb201034
mahmoud kamal <mahmoudkyassin88@gmail.com>
2025-06-30 22:37:56 +0300

    added TopOne Component to view some movie info - add Home Component to be a container

---
+++ src/app/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TopOne } from '../top-one/top-one';
+
+@Component({
+  selector: 'app-home',
+  imports: [TopOne],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
