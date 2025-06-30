955af2efaf9d160bc803e31133e01fdf174cc4a0
Maged Eshak <magedeshak36@gmail.com>
2025-07-01 02:19:52 +0300

    Add regist and login and guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,30 @@
 import { Routes } from '@angular/router';
-import { Register } from './Components/register/register';
-
+import { authGuard } from './guards/auth-guard';
+import { authRequiredGuard } from './guards/auth-required-guard';
 
 export const routes: Routes = [
-    { path: '', component: Register },
-
-
-    //Notfound
-    // { path: 'notFound', component: NotFound, title: 'Notfound' },
-    // { path: '**', redirectTo: 'notFound' }
+    {
+        path: 'login',
+        loadComponent: () => import('./Components/login/login').then(m => m.Login),
+        canActivate: [authGuard]
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./Components/register/register').then(m => m.Register),
+        canActivate: [authGuard]
+    },
+    {
+        path: 'navbar',
+        loadComponent: () => import('./Components/navbar/navbar').then(m => m.Navbar),
+        canActivate: [authRequiredGuard]
+    },
+    {
+        path: '',
+        redirectTo: 'navbar',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
 ];
