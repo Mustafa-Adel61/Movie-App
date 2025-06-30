ac8d087639e807962dcaa92d2a775a4086b8cb83
mahmoud kamal <mahmoudkyassin88@gmail.com>
2025-06-30 22:49:23 +0300

    Added UserAccount Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { UserAccount } from './user-account/user-account';
 
 
 export const routes: Routes = [
     {path:"",redirectTo:"home" , pathMatch:"full"},
     {path:"home",component:Home},
+    {path:"userAccount",component:UserAccount}
 ]
