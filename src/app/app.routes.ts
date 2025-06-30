2594e17073e1b9d58b046321114ea57c212c9766
Maged Eshak <magedeshak36@gmail.com>
2025-06-30 22:54:32 +0300

    env

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Register } from './Components/register/register';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: Register },
+
+
+    //Notfound
+    // { path: 'notFound', component: NotFound, title: 'Notfound' },
+    // { path: '**', redirectTo: 'notFound' }
+];
